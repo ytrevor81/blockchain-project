95f55e8968aa1c853c80a2914ed143df44cea8f1
Trevor Youngquist <ytrevor81@gmail.com>
2022-10-27 23:37:22 +0700

    passed test for transfer

--- test/TrevTokenTest.js
+++ test/TrevTokenTest.js
@@ -1,31 +1,28 @@
 const TrevToken = artifacts.require("TrevToken")
 
 contract("TrevToken", (accounts) => {
-  it ("deployed", async () => {
-    let contractInstance = await TrevToken.deployed();
-    let name = await contractInstance.name();
-    assert.equal(name, "Trev Token", "not the same name")
-  });
-
   it ("successful transfer", async () => {
-    let contractInstance = await TrevToken.deployed();
-    let mainAccount = accounts[0];
-    let receivingAccount = accounts[1];
-    let balanceOfMainAccount = await contractInstance.balanceOf(mainAccount);
-    let totalSupply = await contractInstance.totalSupply();
+    const contractInstance = await TrevToken.deployed();
+    const mainAccount = accounts[0];
+    const receivingAccount = accounts[1];
+    const balanceOfMainAccount = await contractInstance.balanceOf(mainAccount);
+    const totalSupply = await contractInstance.totalSupply();
     assert.equal(BigInt(balanceOfMainAccount), BigInt(totalSupply), "not the same balance");
 
-    let amount = 1000;
-    let complete = await contractInstance.transfer.call(receivingAccount, amount, { from: mainAccount });
-    let balanceOfReceiver = await contractInstance.balanceOf(receivingAccount);
-    assert.equal(balanceOfReceiver.toNumber(), amount, "transfer complete");
+    const amount = 1000;
+    const success = await contractInstance.transfer.call(receivingAccount, amount, { from: mainAccount });
+    assert.equal(success, true, 'transfer is valid');
+    const eventData =  await contractInstance.transfer(receivingAccount, amount, { from: mainAccount });
+    assert.equal(eventData.logs[0].args.value.toNumber(), amount, 'logs the transfer amount');
+    const balanceOfReceiver = await contractInstance.balanceOf(receivingAccount);
+    assert.equal(balanceOfReceiver.toNumber(), amount, "receiving account has expected amount of tokens");
   });
 
-  it ("successful add to blacklist", async () => {
-    let contractInstance = await TrevToken.deployed();
-    let blackListedAccount = accounts[1];
-    await contractInstance.addToBlackList(blackListedAccount);
-    let isBlackListed = await contractInstance.getBlackListedStatus(blackListedAccount);
-    assert.equal(isBlackListed, true, "address not blacklisted");
-  });
+  // it ("successful add to blacklist", async () => {
+  //   let contractInstance = await TrevToken.deployed();
+  //   let blackListedAccount = accounts[1];
+  //   await contractInstance.addToBlackList(blackListedAccount); //should return bool
+  //   let isBlackListed = await contractInstance.getBlackListedStatus(blackListedAccount);
+  //   assert.equal(isBlackListed, true, "address not blacklisted");
+  // });
 });
