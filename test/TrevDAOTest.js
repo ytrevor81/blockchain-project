8b89ca852f8b51ce0cd5389942457f78b3af9ee9
Trevor Youngquist <ytrevor81@gmail.com>
2023-01-20 09:58:40 +0700

    created a string to bytes32 helper class to aid in translating string to bytes32 and vice versa for DAO contract

--- test/TrevDAOTest.js
+++ test/TrevDAOTest.js
@@ -1,15 +1,18 @@
+import StringBytes32Helper from "../scripts/StringBytes32Helper";
 const TrevToken = artifacts.require("TrevToken");
 const TrevDAO = artifacts.require("TrevDAO");
 
 contract("TrevDAO", (accounts) => {
   let daoInstance;
+  let daoAddress;
   let tokenInstance;
+  let tokenAddress;
   const mainAccount = accounts[0];
-  const receivingAccount = accounts[1];
+  const stringBytes32Helper = new StringBytes32Helper();
 
   it ("deployed", async () => {
     tokenInstance = await TrevToken.deployed();
-    const tokenAddress = tokenInstance.address;
+    tokenAddress = tokenInstance.address;
     daoInstance = await TrevDAO.deployed(tokenAddress);
 
     const tokenAddressInDAO = await daoInstance.trevTokenAddress();
@@ -17,12 +20,9 @@ contract("TrevDAO", (accounts) => {
   });
 
 
-  // it ("deployed", async () => {
-  //   tokenInstance = await TrevToken.deployed();
-  //   const tokenAddress = tokenInstance.address;
-  //   daoInstance = await TrevDAO.deployed(tokenAddress);
-  //
-  //   const tokenAddressInDAO = await daoInstance.trevTokenAddress();
-  //   assert.equal(tokenAddressInDAO, tokenAddress, "dao contract ready")
-  // });
+  it ("making a proposal", async () => {
+    daoAddress = daoInstance.address;  
+    
+    //still in progress
+  });
 });
